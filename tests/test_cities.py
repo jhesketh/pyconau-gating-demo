f497e33f9118751487f6f54db9878e1e5e739958
Joshua Hesketh <josh@nitrotech.org>
2015-07-27 17:54:35 +1000

    Add support for states

--- tests/test_cities.py
+++ tests/test_cities.py
@@ -8,3 +8,7 @@ class TestCities(testtools.TestCase):
         largest = 'Sydney'
         self.assertEqual(
             largest, cities.largest_city(cities.get_population_data()).name)
+
+    def test_largest_state(self):
+        largest = 'NSW'
+        self.assertEqual(largest, cities.largest_state(cities.get_population_data()))
